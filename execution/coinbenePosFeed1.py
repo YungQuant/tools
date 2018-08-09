dc1ba4e33ddaf5384d18f56ca8a6c238ff29087d
Hugo Melo <me@hugomelo.com>
2018-08-09 13:12:39 -0400

    fix

--- execution/coinbenePosFeed1.py
+++ execution/coinbenePosFeed1.py
@@ -115,27 +115,27 @@ timeCnt, execTrades = 0, 0
 starttime = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
 
 while (1):
-    # try:
-    orders = get_orderbook(ticker, 10)['orderbook']
-    bid, ask = orders['bids'][0]['price'], orders['asks'][0]['price']
-    midpoint = np.mean([bid, ask])
-    bals = filter_balance(post_balance({ "account": "exchange" }))
+    try:
+        orders = get_orderbook(ticker, 10)['orderbook']
+        bid, ask = orders['bids'][0]['price'], orders['asks'][0]['price']
+        midpoint = np.mean([bid, ask])
+        bals = filter_balance(post_balance({ "account": "exchange" }))
 
-    print("PosFeed Version 1 -yungquant")
-    print("Ticker:", ticker)
-    print("starttime:", starttime)
-    print("balances:", bals)
-    print("price:", midpoint, "\n")
-    if d == 1:
-        pass # print("coinbene doesn't return executed orders")
-    if a == 1:
-        active_orders = post_open_orders({ "symbol": ticker })
-        print("active:", active_orders, "\n")
+        print("PosFeed Version 1 -yungquant")
+        print("Ticker:", ticker)
+        print("starttime:", starttime)
+        print("balances:", bals)
+        print("price:", midpoint, "\n")
+        if d == 1:
+            pass # print("coinbene doesn't return executed orders")
+        if a == 1:
+            active_orders = post_open_orders({ "symbol": ticker })
+            print("active:", active_orders, "\n")
 
-    time.sleep(1)
-    timeCnt += 1
-    print("timeCnt:", timeCnt, ",", timeCnt / 60, "minutes\n")
-    # except:
-    # print("FUUUUUUUUUUCK", sys.exc_info())
-    # time.sleep(1)
+        time.sleep(1)
+        timeCnt += 1
+        print("timeCnt:", timeCnt, ",", timeCnt / 60, "minutes\n")
+    except:
+        print("FUUUUUUUUUUCK", sys.exc_info())
+        time.sleep(1)
 
