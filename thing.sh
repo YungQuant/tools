fc0ce51b860e3a004062e7dbbb075b356959642c
fubar <theboss723@gmail.com>
2018-08-23 02:39:37 -0400

    update

--- thing.sh
+++
@@ -1,9 +0,0 @@
-git filter-branch --commit-filter '
-      if [ "$GIT_AUTHOR_EMAIL" = "*" ];
-      then
-              GIT_AUTHOR_NAME="yungquant";
-              GIT_AUTHOR_EMAIL="theboss723@gmail.com";
-              git commit-tree "$@";
-      else
-              git commit-tree "$@";
-      fi' HEAD
